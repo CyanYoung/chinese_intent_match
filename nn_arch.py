c706cd22be6d00a8aa84f90680d6197cfa6bf77b
yangjc <bupt_yjc@163.com>
2019-02-25 10:49:15 +0800

    change arch

--- nn_arch.py
+++ nn_arch.py
@@ -1,89 +1,117 @@
-from keras.layers import Dense, SeparableConv1D, LSTM
-from keras.layers import GlobalMaxPooling1D, Masking, Lambda, Dot, Concatenate
+from keras.layers import Dense, Conv1D, Conv2D, LSTM, Lambda
+from keras.layers import Dropout, GlobalMaxPooling1D, GlobalMaxPooling2D, Masking, Flatten
+from keras.layers import Permute, Reshape, Concatenate, Subtract, Multiply, Dot
 
 import keras.backend as K
 
 
-def dnn(embed_input1, embed_input2):
-    mean = Lambda(lambda a: K.mean(a, axis=1), name='mean')
-    da1 = Dense(200, activation='relu', name='encode1')
-    da2 = Dense(200, activation='relu', name='encode2')
+seq_len = 30
+
+
+def dnn_siam(embed_input1, embed_input2):
+    mean = Lambda(lambda a: K.mean(a, axis=1))
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(200, activation='relu')
+    da3 = Dense(1, activation='sigmoid')
     x = mean(embed_input1)
     x = da1(x)
     x = da2(x)
     y = mean(embed_input2)
     y = da1(y)
     y = da2(y)
-    return Dot(1, normalize=True)([x, y])
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = Concatenate()([x, y, diff, prod])
+    z = Dropout(0.2)(z)
+    return da3(z)
 
 
-def dnn_encode(embed_input):
-    mean = Lambda(lambda a: K.mean(a, axis=1), name='mean')
-    da1 = Dense(200, activation='relu', name='encode1')
-    da2 = Dense(200, activation='relu', name='encode2')
-    x = mean(embed_input)
+def dnn_join(embed_input1, embed_input2):
+    da1 = Dense(500, activation='relu')
+    da2 = Dense(200, activation='relu')
+    da3 = Dense(1, activation='sigmoid')
+    dot_input = Dot(2)([embed_input1, embed_input2])
+    x = Flatten()(dot_input)
     x = da1(x)
-    return da2(x)
+    x = da2(x)
+    x = Dropout(0.2)(x)
+    return da3(x)
 
 
-def cnn(embed_input1, embed_input2):
-    ca1 = SeparableConv1D(filters=64, kernel_size=1, padding='same', activation='relu', name='conv1')
-    ca2 = SeparableConv1D(filters=64, kernel_size=2, padding='same', activation='relu', name='conv2')
-    ca3 = SeparableConv1D(filters=64, kernel_size=3, padding='same', activation='relu', name='conv3')
+def cnn_siam(embed_input1, embed_input2):
+    ca1 = Conv1D(filters=64, kernel_size=1, padding='same', activation='relu')
+    ca2 = Conv1D(filters=64, kernel_size=2, padding='same', activation='relu')
+    ca3 = Conv1D(filters=64, kernel_size=3, padding='same', activation='relu')
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(1, activation='sigmoid')
     mp = GlobalMaxPooling1D()
-    concat = Concatenate()
-    da = Dense(200, activation='relu', name='encode')
+    concat1 = Concatenate()
+    concat2 = Concatenate()
     x1 = ca1(embed_input1)
     x1 = mp(x1)
     x2 = ca2(embed_input1)
     x2 = mp(x2)
     x3 = ca3(embed_input1)
     x3 = mp(x3)
-    x = concat([x1, x2, x3])
-    x = da(x)
+    x = concat1([x1, x2, x3])
     y1 = ca1(embed_input2)
     y1 = mp(y1)
     y2 = ca2(embed_input2)
     y2 = mp(y2)
     y3 = ca3(embed_input2)
     y3 = mp(y3)
-    y = concat([y1, y2, y3])
-    y = da(y)
-    return Dot(1, normalize=True)([x, y])
+    y = concat1([y1, y2, y3])
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = concat2([x, y, diff, prod])
+    z = da1(z)
+    z = Dropout(0.2)(z)
+    return da2(z)
 
 
-def cnn_encode(embed_input):
-    ca1 = SeparableConv1D(filters=64, kernel_size=1, padding='same', activation='relu', name='conv1')
-    ca2 = SeparableConv1D(filters=64, kernel_size=2, padding='same', activation='relu', name='conv2')
-    ca3 = SeparableConv1D(filters=64, kernel_size=3, padding='same', activation='relu', name='conv3')
-    mp = GlobalMaxPooling1D()
-    da = Dense(200, activation='relu', name='encode')
-    x1 = ca1(embed_input)
+def cnn_join(embed_input1, embed_input2):
+    ca1 = Conv2D(filters=64, kernel_size=1, padding='same', activation='relu')
+    ca2 = Conv2D(filters=64, kernel_size=2, padding='same', activation='relu')
+    ca3 = Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')
+    da1 = Dense(200, activation='relu')
+    da2 = Dense(1, activation='sigmoid')
+    mp = GlobalMaxPooling2D()
+    dot_input = Dot(2)([embed_input1, embed_input2])
+    dot_input = Reshape((seq_len, seq_len, 1))(dot_input)
+    x1 = ca1(dot_input)
     x1 = mp(x1)
-    x2 = ca2(embed_input)
+    x2 = ca2(dot_input)
     x2 = mp(x2)
-    x3 = ca3(embed_input)
+    x3 = ca3(dot_input)
     x3 = mp(x3)
     x = Concatenate()([x1, x2, x3])
-    return da(x)
+    x = da1(x)
+    x = Dropout(0.2)(x)
+    return da2(x)
 
 
-def rnn(embed_input1, embed_input2):
+def rnn_siam(embed_input1, embed_input2):
     mask = Masking()
-    ra = LSTM(200, activation='tanh', name='encode1')
-    da = Dense(200, activation='relu', name='encode2')
+    ra = LSTM(200, activation='tanh')
+    da = Dense(1, activation='sigmoid')
     x = mask(embed_input1)
     x = ra(x)
-    x = da(x)
     y = mask(embed_input2)
     y = ra(y)
-    y = da(y)
-    return Dot(1, normalize=True)([x, y])
+    diff = Lambda(lambda a: K.abs(a))(Subtract()([x, y]))
+    prod = Multiply()([x, y])
+    z = Concatenate()([x, y, diff, prod])
+    z = Dropout(0.2)(z)
+    return da(z)
 
 
-def rnn_encode(embed_input):
-    ra = LSTM(200, activation='tanh', name='encode1')
-    da = Dense(200, activation='relu', name='encode2')
-    x = Masking()(embed_input)
-    x = ra(x)
+def rnn_join(embed_input1, embed_input2):
+    ra = LSTM(30, activation='tanh')
+    da = Dense(1, activation='sigmoid')
+    dot_input1 = Dot(2)([embed_input1, embed_input2])
+    dot_input2 = Permute((2, 1))(dot_input1)
+    x1 = ra(dot_input1)
+    x2 = ra(dot_input2)
+    x = Concatenate()([x1, x2])
+    x = Dropout(0.2)(x)
     return da(x)
